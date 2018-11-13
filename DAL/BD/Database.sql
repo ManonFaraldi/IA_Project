2916595028c6117ebe9a34219d6b4405eaea7c0a
Manon <36883061+ManonFaraldi@users.noreply.github.com>
2018-11-13 15:03:17 +0100

    Delete Database.sql

--- DAL/BD/Database.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE database if not exists projetIA character set utf8 collate utf8_unicode_ci;
-use projetIA;
-
-grant all privileges on projetIA.* to 'projetIA_user'@'localhost' identified by 'secret';
