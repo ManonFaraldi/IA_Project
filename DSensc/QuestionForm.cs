8cfea3bbbbf7d4b3c95b71a623671bc1350efd01
LeaDgst <36511228+LeaDgst@users.noreply.github.com>
2018-11-23 18:56:24 +0100

    fonction Vrai Faux initialisée

--- DSensc/QuestionForm.cs
+++ DSensc/QuestionForm.cs
@@ -16,10 +16,13 @@ namespace App
         public List<Questions> questions = new List<Questions>();
 
         private int NbQuestion { get; set; }   
+        private int NumRep { get; set; }
       
         public QuestionForm()
         {
             InitializeComponent();
+
+            MainForm.ActiveForm.Hide();
 
             // Chargement des questions du fichier xml dans Questions
             questions = SerialisationQuestions.CreateFromFile("..\\..\\..\\Donnees\\Questions.xml");
@@ -35,7 +38,6 @@ namespace App
             rep3_radiobtn.Text = questions[NbQuestion].Reponse3;
             rep4_radiobtn.Text = questions[NbQuestion].Reponse4;
 
-            // prenom_txt.Text = MainForm.ActiveForm.Text.Nom; // AREVOIR !!!!!
         }
 
 
@@ -50,14 +52,14 @@ namespace App
             {
                 NbQuestion++;
 
-                if (NbQuestion == 20) // le test est fini
+                if (NbQuestion == 20) //le test est fini
                 {
                     ResultatsForm resultatsForm = new ResultatsForm();
                     if (resultatsForm.ShowDialog() == DialogResult.OK)
                     {
                     }
                 }
-                else //Affichage du n° et texte de la question :
+                else //affichage du n° et texte de la question :
                 {                 
                     numQuestion_lbl.Text = Convert.ToString(NbQuestion + 1);
                     question_enonce_lbl.Text = questions[NbQuestion].Enonce;
@@ -76,7 +78,7 @@ namespace App
         }
 
 
-        public static void TriQuestion<T>(IList<T> list)
+        public static void TriQuestion<T>(IList<T> list) //Trier les questions dans un ordre aléatoire
         {
             Random random = new Random();
             int n = list.Count;
@@ -89,5 +91,30 @@ namespace App
                 list[i] = value;
             }
         }
+
+        public void VraiFaux(int numQuestion) //Evaluer si la réponse cochée est vraie ou fausse
+        {
+            //Convertir la réponse cochée en n° :
+            if(rep1_radiobtn.Checked == true) { NumRep = 1; }
+            else if (rep2_radiobtn.Checked == true) { NumRep = 2; }
+            else if (rep3_radiobtn.Checked == true) { NumRep = 3; }
+            else if (rep4_radiobtn.Checked == true) //Mettre un else --> fonctionne toujours ?????????????????????????????????????????????????????????????????????????????????????
+            {
+                NumRep = 4;
+            }
+
+
+
+            //Affichage en vert de la bonne réponse et en rouge de la mauvaise réponse :
+            if (NumRep == questions[numQuestion].ReponseVraie)
+            {
+                
+            }
+
+        }
+
+
+
+
     }
 }
